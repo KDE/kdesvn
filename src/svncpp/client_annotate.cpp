d111195b5964d2b85b14b0b3fbdd270cc6fc8d8e
Rajko Albrecht <ral@alwins-world.de>
2005-07-19 09:09:20 +0000

    Fixed a crasher when blaming a range send fix back to rapidsvn

--- src/svncpp/client_annotate.cpp
+++ src/svncpp/client_annotate.cpp
@@ -6,15 +6,15 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
- * License along with this library (in the file LGPL.txt); if not, 
- * write to the Free Software Foundation, Inc., 51 Franklin St, 
+ * License along with this library (in the file LGPL.txt); if not,
+ * write to the Free Software Foundation, Inc., 51 Franklin St,
  * Fifth Floor, Boston, MA  02110-1301  USA
  *
  * This software consists of voluntary contributions made by many
@@ -45,14 +45,14 @@ namespace svn
                     apr_pool_t *pool)
   {
     AnnotatedFile * entries = (AnnotatedFile *) baton;
-    entries->push_back( AnnotateLine (line_no, revision, author, date, line));
+    entries->push_back( AnnotateLine (line_no, revision, author?author:"unknown", date?date:"unknown date", line?line:"???"));
 
     return NULL;
   }
 
   AnnotatedFile *
-  Client::annotate (const Path & path, 
-            const Revision & revisionStart, 
+  Client::annotate (const Path & path,
+            const Revision & revisionStart,
             const Revision & revisionEnd) throw (ClientException)
   {
     Pool pool;
@@ -60,10 +60,10 @@ namespace svn
     svn_error_t *error;
     error = svn_client_blame (
       path.c_str (),
-      revisionStart.revision (), 
-      revisionEnd.revision (), 
+      revisionStart.revision (),
+      revisionEnd.revision (),
       annotateReceiver,
-      entries, 
+      entries,
       *m_context, // client ctx
       pool);
 

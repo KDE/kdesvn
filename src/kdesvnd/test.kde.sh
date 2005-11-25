f841e599d6012538ffc114fc2fb67ae4f1dfe903
Rajko Albrecht <ral@alwins-world.de>
2005-11-25 11:56:15 +0000

    context menues will modified in respect for which kde version it is installed (3.5 vs. 3.4 or earlier)

---
+++ src/kdesvnd/test.kde.sh
@@ -0,0 +1,23 @@
+#/bin/sh
+
+j=`kde-config --version|grep KDE|awk '{print $2}'|sed s/\\\\./\\ /g`
+ma=`echo $j | awk '{print $1}'`
+mi=`echo $j | awk '{print $2}'`
+
+echo "KDE Minor = ${mi} und KDE Major = ${ma}"
+
+
+if [ "x$1" = "x" ]; then
+   echo "No input"
+   exit 0
+fi
+echo "Edit ${1}"
+n=`echo ${1}|sed s/\\\\.template//g`
+echo "Moving to ${n}"
+
+if [ "${mi}" = "5" ]; then
+  cat ${1} | sed s/^Actions/\#Actions/g > $n
+else
+  cp ${1} ${n}
+fi
+

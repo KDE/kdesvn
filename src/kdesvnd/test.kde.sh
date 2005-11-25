2bb431957664720c2b60d547060c65e0c8f413bc
Rajko Albrecht <ral@alwins-world.de>
2005-11-25 16:43:34 +0000

    fixed some build problems while generating the desktop-files for context menues

--- src/kdesvnd/test.kde.sh
+++ src/kdesvnd/test.kde.sh
@@ -12,7 +12,13 @@ if [ "x$1" = "x" ]; then
    exit 0
 fi
 echo "Edit ${1}"
-n=`echo ${1}|sed s/\\\\.template//g`
+
+if [ "x$2" = "x" ]; then
+ n=`echo ${1}|sed s/\\\\.template//g`
+else
+ n="${2}"
+fi
+
 echo "Moving to ${n}"
 
 if [ "${mi}" = "5" ]; then

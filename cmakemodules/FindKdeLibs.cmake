e5fadce0e2e9a4b1a01bbd0f13189f4e2b408079
Michael Biebl <biebl@teco.edu>
2006-08-31 22:26:22 +0000

    - The way uic is called by cmake is currently broken [1].   Added a fixed kde3uic and KDE3Macros files from CMake CVS.   This will be necessary until CMake 2.4.4 is released. - Remove the absolute paths in the INCLUDE calls, so that we can override it   with our own copies. Added ${CMAKE_SOURCE_DIR}/cmakemodules to the   CMAKE_MODULE_PATH. - Move FindSubversion.cmake to the toplevel cmakemodules dir. It's better to   have a central place where we store cmake files.
    [1] http://mail.kde.org/pipermail/kde-buildsystem/2006-August/003331.html

--- cmakemodules/FindKdeLibs.cmake
+++ cmakemodules/FindKdeLibs.cmake
@@ -1,6 +1,6 @@
-include(${CMAKE_ROOT}/Modules/FindQt3.cmake)
-include(${CMAKE_ROOT}/Modules/FindKDE3.cmake)
-include(${CMAKE_SOURCE_DIR}/cmakemodules/kdesvnMacros.cmake)
+include(FindQt3)
+include(FindKDE3)
+include(kdesvnMacros)
 
 FIND_LIBRARY(KDE3_UI_LIBRARY NAMES kdeui
   PATHS

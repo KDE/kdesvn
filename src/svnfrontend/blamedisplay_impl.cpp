e941f84bf5357dc42a29275375cb9e4bf4d6c2d6
Rajko Albrecht <ral@alwins-world.de>
2006-10-13 08:04:03 +0000

    some improvements to blamedisplay

--- src/svnfrontend/blamedisplay_impl.cpp
+++ src/svnfrontend/blamedisplay_impl.cpp
@@ -9,17 +9,17 @@
 #include <qpixmap.h>
 #include <qpainter.h>
 
-#define COL_DATE 0
+#define COL_LINENR 0
 #define COL_REV 1
-#define COL_AUT 2
-#define COL_LINENR 3
+#define COL_DATE 2
+#define COL_AUT 3
 #define COL_LINE 4
 
 class BlameDisplayItem:public KListViewItem
 {
 public:
-    BlameDisplayItem(KListView*,const svn::AnnotateLine&);
-    BlameDisplayItem(KListView*,BlameDisplayItem*,const svn::AnnotateLine&);
+    BlameDisplayItem(KListView*,const svn::AnnotateLine&,bool,bool,bool);
+    BlameDisplayItem(KListView*,BlameDisplayItem*,const svn::AnnotateLine&,bool,bool,bool);
     virtual ~BlameDisplayItem(){}
     virtual int compare(QListViewItem *i, int col, bool ascending)const;
     virtual void paintCell(QPainter *p, const QColorGroup &cg, int column, int width, int alignment);
@@ -30,34 +30,38 @@ public:
 protected:
     svn::AnnotateLine m_Content;
 
+    bool m_author,m_date,m_rev;
+
     void display();
 };
 
-BlameDisplayItem::BlameDisplayItem(KListView*lv,const svn::AnnotateLine&al)
-    : KListViewItem(lv),m_Content(al)
+BlameDisplayItem::BlameDisplayItem(KListView*lv,const svn::AnnotateLine&al,bool a,bool d,bool r)
+    : KListViewItem(lv),m_Content(al),m_author(a),m_date(d),m_rev(r)
 {
     display();
 }
 
-BlameDisplayItem::BlameDisplayItem(KListView*lv,BlameDisplayItem*it,const svn::AnnotateLine&al)
-    : KListViewItem(lv,it),m_Content(al)
+BlameDisplayItem::BlameDisplayItem(KListView*lv,BlameDisplayItem*it,const svn::AnnotateLine&al,bool a,bool d,bool r)
+    : KListViewItem(lv,it),m_Content(al),m_author(a),m_date(d),m_rev(r)
 {
     display();
 }
+
+#define BORDER 4
 
 int BlameDisplayItem::width (const QFontMetrics & fm, const QListView * lv, int c ) const
 {
     if (c == COL_LINE) {
-        return KListViewItem::width(QFontMetrics(KGlobalSettings::fixedFont()),lv,c);
+        return KListViewItem::width(QFontMetrics(KGlobalSettings::fixedFont()),lv,c)+2*BORDER;
     }
-    return KListViewItem::width(fm,lv,c);
+    return KListViewItem::width(fm,lv,c)+2*BORDER;
 }
 
 void BlameDisplayItem::display()
 {
-    setText(COL_REV,QString("%1").arg(m_Content.revision()));
-    setText(COL_AUT,m_Content.author());
-    setText(COL_DATE,KGlobal::locale()->formatDateTime(m_Content.date()));
+    if (m_rev) setText(COL_REV,QString("%1").arg(m_Content.revision()));
+    if (m_author) setText(COL_AUT,m_Content.author());
+    if (m_date) setText(COL_DATE,KGlobal::locale()->formatDateTime(m_Content.date()));
     setText(COL_LINENR,QString("%1").arg(m_Content.lineNumber()+1));
     setText(COL_LINE,QString("%1").arg(m_Content.line()));
 }
@@ -71,9 +75,9 @@ int BlameDisplayItem::compare(QListViewItem *item, int col, bool ascending)const
     }
     if (col == COL_AUT) {
         if (Kdesvnsettings::locale_is_casesensitive()) {
-            return text(col).localeAwareCompare(k->text(col));
+            return m_Content.author().localeAwareCompare(k->m_Content.author());
         }
-        return text(col).compare(k->text(col));
+        return m_Content.author().compare(k->m_Content.author());
     }
     return k->m_Content.lineNumber()-m_Content.lineNumber();
 }
@@ -89,13 +93,14 @@ void BlameDisplayItem::paintCell(QPainter *p, const QColorGroup &cg, int column,
     QColorGroup _cg = cg;
     QColor _bgColor;
     if (column==COL_LINENR) {
-        _bgColor = QColor(230,230,230);
+        _bgColor = KGlobalSettings::highlightColor();
+        p->setPen(KGlobalSettings::highlightedTextColor());
     } else if (column==COL_DATE ){
         _bgColor = QColor(210,230,230);
     } else if (column==COL_REV ){
         _bgColor = QColor(230,230,210);
     } else if (column==COL_AUT ){
-        _bgColor = QColor(230,210,230);
+        _bgColor = QColor(230,230,230);
     } else {
         KListViewItem::paintCell(p,cg,column,width,alignment);
         return;
@@ -112,7 +117,14 @@ void BlameDisplayItem::paintCell(QPainter *p, const QColorGroup &cg, int column,
             _cg.setColor(QColorGroup::Base,_bgColor);
         }
     }
-    QListViewItem::paintCell(p, _cg, column, width, alignment);
+    p->fillRect(0, 0, width, height(), _bgColor);
+    QString str = text(column);
+    if (str.isEmpty())
+        return;
+    if (alignment & (AlignTop || AlignBottom) == 0)
+        alignment |= AlignVCenter;
+    p->drawText(BORDER, 0, width - 2*BORDER, height(), alignment, str);
+    //QListViewItem::paintCell(p, _cg, column, width, alignment);
 }
 
 class BlameDisplayData
@@ -145,10 +157,21 @@ void BlameDisplay_impl::setContent(const svn::AnnotatedFile&blame)
     svn::AnnotatedFile::const_iterator bit;
     m_BlameList->setSorting(COL_LINENR,false);
     m_Data->max = -1;
+    QDateTime lastDate;
+    QString lastAuthor;
+    svn_revnum_t lastRev(-1);
     for (bit=blame.begin();bit!=blame.end();++bit) {
+        bool disp =
+                (*bit).author()!=lastAuthor ||
+                (*bit).date()!=lastDate ||
+                (*bit).revision()!=lastRev;
+
         if ((*bit).revision()>m_Data->max) m_Data->max=(*bit).revision();
         if ((*bit).revision()<m_Data->min) m_Data->min=(*bit).revision();
-        new BlameDisplayItem(m_BlameList,(*bit));
+        new BlameDisplayItem(m_BlameList,(*bit),disp,disp,disp);
+        lastRev = (*bit).revision();
+        lastDate = (*bit).date();
+        lastAuthor = (*bit).author();
     }
     kdDebug()<<"Max revision: "<<m_Data->max << endl;
     kdDebug()<<"Min revision: "<<m_Data->min << endl;

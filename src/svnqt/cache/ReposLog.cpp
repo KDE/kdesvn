67f019a11b262800ce90087f5882d445b9c11541
Rajko Albrecht <ral@alwins-world.de>
2008-06-20 13:46:41 +0000

    cached log now lists changed entries, too

--- src/svnqt/cache/ReposLog.cpp
+++ src/svnqt/cache/ReposLog.cpp
@@ -399,6 +399,9 @@ bool svn::cache::ReposLog::insertLogEntry(const svn::LogEntry&aEntry)
 bool svn::cache::ReposLog::log(const svn::Path&what,const svn::Revision&_start, const svn::Revision&_end,const svn::Revision&_peg,svn::LogEntriesMap&target, bool strictNodeHistory,int limit)
 {
     static QString s_q("select logentries.revision,logentries.author,logentries.date,logentries.message from logentries where logentries.revision in (select changeditems.revision from changeditems where (changeditems.changeditem='%1' or changeditems.changeditem GLOB '%2/*') %3 GROUP BY changeditems.revision) ORDER BY logentries.revision DESC");
+
+    static QString s_e("select changeditem,action,copyfrom,copyfromrev from changeditems where changeditems.revision='%1'");
+
     svn::Revision peg = date2numberRev(_peg,true);
     svn::Revision end = date2numberRev(_end,true);
     svn::Revision start = date2numberRev(_start,true);
@@ -414,6 +417,7 @@ bool svn::cache::ReposLog::log(const svn::Path&what,const svn::Revision&_start, 
     }
     qDebug("Query-string: %s",query_string.TOUTF8().data());
     QSqlQuery _q(QString::null,m_Database);
+    QSqlQuery _q2(QString::null,m_Database);
     _q.prepare(query_string);
     if (!_q.exec()) {
         qDebug("Could not select values: %s",_q.lastError().text().TOUTF8().data());
@@ -426,6 +430,23 @@ bool svn::cache::ReposLog::log(const svn::Path&what,const svn::Revision&_start, 
         target[revision].author=_q.value(1).toString();
         target[revision].date=_q.value(2).toLongLong();
         target[revision].message=_q.value(3).toString();
+        query_string=s_e.arg(revision);
+        qDebug("Query-string: %s",query_string.TOUTF8().data());
+        _q2.prepare(query_string);
+        if (!_q2.exec()) {
+            qDebug("Could not select values: %s",_q2.lastError().text().TOUTF8().data());
+        } else {
+            while (_q2.next()) {
+                target[revision].changedPaths.push_back (
+                        LogChangePathEntry (_q2.value(0).toString(),
+                                            _q2.value(1).toString()[0],
+                                            _q2.value(2).toString(),
+                                            _q2.value(3).toLongLong()
+                                           )
+                                                        );
+            }
+        }
+
     }
     return true;
 }

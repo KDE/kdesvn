5c094bce2cc82dbeb92cac0c28a78cfd7273a5a6
Rajko Albrecht <ral@alwins-world.de>
2008-07-30 21:56:43 +0000

    * revision trees simplelog checks the nonetwork setting

--- src/svnfrontend/graphtree/revisiontree.cpp
+++ src/svnfrontend/graphtree/revisiontree.cpp
@@ -27,6 +27,7 @@
 #include "revgraphview.h"
 #include "elogentry.h"
 #include "src/svnfrontend/fronthelpers/cursorstack.h"
+#include "src/settings/kdesvnsettings.h"
 
 #include <kdebug.h>
 #include <kprogress.h>
@@ -95,7 +96,7 @@ bool RtreeData::getLogs(const QString&reposRoot,const svn::Revision&startr,const
                      0,"Logs",i18n("Getting logs - hit cancel for abort"));
         //m_Client->log(reposRoot,endr,startr,m_OldHistory,startr,true,false,0);
         svn::cache::ReposLog rl(m_Client,reposRoot);
-        rl.simpleLog(m_OldHistory,startr,endr,true);
+        rl.simpleLog(m_OldHistory,startr,endr,!Kdesvnsettings::network_on());
     } catch (const svn::Exception&ce) {
         kdDebug()<<ce.msg() << endl;
         KMessageBox::error(0,i18n("Could not retrieve logs, reason:\n%1").arg(ce.msg()));

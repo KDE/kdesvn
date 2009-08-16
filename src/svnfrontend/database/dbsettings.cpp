75dab547e6aff9829b844b721ba21850b836bd06
Rajko Albrecht <ral@alwins-world.de>
2009-08-16 21:14:31 +0000

    After hints from Micheal Biebl switched over to dual license: Kdesvn is GPL 2 and svnqt list LGPL 2.1 licensed. Hopefully all source files have copyright header.

--- src/svnfrontend/database/dbsettings.cpp
+++ src/svnfrontend/database/dbsettings.cpp
@@ -3,17 +3,17 @@
  *   http://kdesvn.alwins-world.de/                                        *
  *                                                                         *
  * This program is free software; you can redistribute it and/or           *
- * modify it under the terms of the GNU Lesser General Public              *
+ * modify it under the terms of the GNU General Public              *
  * License as published by the Free Software Foundation; either            *
  * version 2.1 of the License, or (at your option) any later version.      *
  *                                                                         *
  * This program is distributed in the hope that it will be useful,         *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       *
- * Lesser General Public License for more details.                         *
+ * General Public License for more details.                         *
  *                                                                         *
- * You should have received a copy of the GNU Lesser General Public        *
- * License along with this program (in the file LGPL.txt); if not,         *
+ * You should have received a copy of the GNU General Public        *
+ * License along with this program (in the file GPL.txt); if not,         *
  * write to the Free Software Foundation, Inc., 51 Franklin St,            *
  * Fifth Floor, Boston, MA  02110-1301  USA                                *
  *                                                                         *

a55ca389f96e94da8aa7ebe00b8d82c7db134d09
Rajko Albrecht <ral@alwins-world.de>
2012-06-12 13:22:42 +0000

    removed support for subversion 1.4 or older from blame/annotate, so kdesvn/svnqt requires subversion 1.5 or newer

--- src/svnqt/client_annotate.cpp
+++ src/svnqt/client_annotate.cpp
@@ -42,7 +42,6 @@
 
 namespace svn
 {
-#if ((SVN_VER_MAJOR == 1) && (SVN_VER_MINOR >= 5)) || (SVN_VER_MAJOR > 1)
     static svn_error_t *
             annotateReceiver(void *baton,
                               apr_int64_t line_no,
@@ -62,33 +61,12 @@ namespace svn
                             merge_author,merge_date,merge_path));
         return NULL;
     }
-#else
-  static svn_error_t *
-  annotateReceiver(void *baton,
-                    apr_int64_t line_no,
-                    svn_revnum_t revision,
-                    const char *author,
-                    const char *date,
-                    const char *line,
-                    apr_pool_t *)
-  {
-    AnnotatedFile * entries = (AnnotatedFile *) baton;
-    entries->push_back (
-      AnnotateLine (line_no, revision,
-                    author?author:"",
-                    date?date:"",
-                    line?line:""));
-
-    return NULL;
-  }
-#endif
 
   void
   Client_impl::annotate (AnnotatedFile&target,const AnnotateParameter&params) throw (ClientException)
   {
     Pool pool;
     svn_error_t *error;
-#if ((SVN_VER_MAJOR == 1) && (SVN_VER_MINOR >= 5)) || (SVN_VER_MAJOR > 1)
     error = svn_client_blame4(
                 params.path().cstr(),
                 params.pegRevision().revision(),
@@ -101,19 +79,6 @@ namespace svn
                 &target,
                 *m_context, // client ctx
                 pool);
-#elif ((SVN_VER_MAJOR == 1) && (SVN_VER_MINOR >= 4)) || (SVN_VER_MAJOR > 1)
-    error = svn_client_blame3(
-                params.path().cstr(),
-                params.pegRevision().revision(),
-                params.revisionRange().first,
-                params.revisionRange().second,
-                params.diffOptions().options(pool),
-                params.ignoreMimeTypes(),
-                annotateReceiver,
-                &target,
-                *m_context, // client ctx
-                pool);
-#endif
     if (error != NULL)
     {
       throw ClientException (error);

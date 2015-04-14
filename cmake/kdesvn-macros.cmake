d7994ed4003aa7bed77a2fb29d31ea0420f6bcc6
Ovidiu-Florin BOGDAN <ovidiu.b13@gmail.com>
2015-04-14 16:58:01 +0300

    Update CMakeLists.txt for KF5

--- cmake/kdesvn-macros.cmake
+++ cmake/kdesvn-macros.cmake
@@ -5,7 +5,7 @@ macro(BUILD_TEST tname)
         set(${tname}-src ${${tname}-src} ${tname}.h)
   endif()
   add_executable(${tname} ${${tname}-src})
-  target_link_libraries(${tname} svnqt ${QT_QTCORE_LIBRARY})
+  target_link_libraries(${tname} svnqt Qt5::Core Qt5::Sql)
   add_test(${tname} ${CMAKE_CURRENT_BINARY_DIR}/${tname})
 endmacro()
 
